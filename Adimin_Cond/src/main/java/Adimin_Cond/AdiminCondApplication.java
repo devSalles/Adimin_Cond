a3e14cc2904da056a91ea6d6a245aed662394790
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2026-01-14 16:22:45 -0300

    Upload projeto

---
+++ Adimin_Cond/src/main/java/Adimin_Cond/AdiminCondApplication.java
@@ -0,0 +1,13 @@
+package Adimin_Cond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiminCondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdiminCondApplication.class, args);
+	}
+
+}
---
+++ Adimin_Cond/src/test/java/Adimin_Cond/AdiminCondApplicationTests.java
@@ -0,0 +1,13 @@
+package Adimin_Cond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiminCondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
