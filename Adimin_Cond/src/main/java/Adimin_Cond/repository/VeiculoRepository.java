b1c015ed6a7befc71a10419960dff9bc52c79a1f
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-02-01 14:53:31 -0300

    Desenvolvimento de metodos de pesquisa e existência de placa de veiculo

--- Adimin_Cond/src/main/java/Adimin_Cond/repository/VeiculoRepository.java
+++ Adimin_Cond/src/main/java/Adimin_Cond/repository/VeiculoRepository.java
@@ -1,9 +1,20 @@
 package Adimin_Cond.repository;
 
+import Adimin_Cond.Enum.StatusVeiculo;
+import Adimin_Cond.dto.veiculo.VeiculoRequestDTO;
 import Adimin_Cond.entity.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo,Long> {
+
+    boolean existsByPlaca(String placa);
+
+    Veiculo findByPlaca(String placa);
+
+    List<Veiculo> findByStatus(StatusVeiculo statusVeiculo);
+
 }
