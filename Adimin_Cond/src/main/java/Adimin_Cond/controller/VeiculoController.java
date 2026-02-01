55e409a5583943d2a9cd14e306963a5549f7bba0
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-02-01 14:58:45 -0300

    Desenvolvimento de service e controller de veículo (implementação de metodos de buscar, adição, atualização e deleção)

--- Adimin_Cond/src/main/java/Adimin_Cond/controller/VeiculoController.java
+++ Adimin_Cond/src/main/java/Adimin_Cond/controller/VeiculoController.java
@@ -1,11 +1,65 @@
 package Adimin_Cond.controller;
 
+import Adimin_Cond.Enum.StatusVeiculo;
+import Adimin_Cond.dto.veiculo.VeiculoRequestDTO;
+import Adimin_Cond.dto.veiculo.VeiculoResponseDTO;
+import Adimin_Cond.dto.veiculo.VeiculoUpdateRequestDTO;
+import Adimin_Cond.service.VeiculoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/veiculos")
 @RequiredArgsConstructor
+@Tag(name = "Veículo")
 public class VeiculoController {
+
+    private final VeiculoService veiculoService;
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<VeiculoResponseDTO> criar(@Valid @RequestBody VeiculoRequestDTO dto) {
+        VeiculoResponseDTO response = veiculoService.salvarVeiculo(dto);
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<VeiculoResponseDTO> atualizar(@PathVariable Long id, @Valid @RequestBody VeiculoUpdateRequestDTO dto) {
+        VeiculoResponseDTO response = veiculoService.atualizarVeiculo(id, dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/buscar-placa/{placa}")
+    public ResponseEntity<VeiculoResponseDTO> buscarPorPlaca(@PathVariable String placa) {
+        VeiculoResponseDTO response = veiculoService.buscarPlaca(placa);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/listar-todos")
+    public ResponseEntity<List<VeiculoResponseDTO>> listarTodos() {
+        List<VeiculoResponseDTO> response = veiculoService.listarTodos();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<VeiculoResponseDTO> buscarPorId(@PathVariable Long id) {
+        VeiculoResponseDTO response = veiculoService.veiculoID(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/listar-por-status/{status}")
+    public ResponseEntity<List<VeiculoResponseDTO>> listarPorStatus(@PathVariable StatusVeiculo status) {
+        List<VeiculoResponseDTO> response = veiculoService.listarPorStatus(status);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/desativar/{id}")
+    public ResponseEntity<VeiculoResponseDTO> desativarVeiculo(@PathVariable Long id) {
+        VeiculoResponseDTO veiculo = veiculoService.desativarVeiculo(id);
+        return ResponseEntity.ok(veiculo);
+    }
 }
--- Adimin_Cond/src/main/java/Adimin_Cond/service/VeiculoService.java
+++ Adimin_Cond/src/main/java/Adimin_Cond/service/VeiculoService.java
@@ -1,9 +1,118 @@
 package Adimin_Cond.service;
 
+import Adimin_Cond.Enum.StatusVeiculo;
+import Adimin_Cond.core.exception.IdNaoEncontradoException;
+import Adimin_Cond.core.exception.NenhumCadastroException;
+import Adimin_Cond.core.exception.PlacaRepetidaException;
+import Adimin_Cond.core.exception.VeiculoInativoException;
+import Adimin_Cond.dto.veiculo.VeiculoRequestDTO;
+import Adimin_Cond.dto.veiculo.VeiculoResponseDTO;
+import Adimin_Cond.dto.veiculo.VeiculoUpdateRequestDTO;
+import Adimin_Cond.entity.Veiculo;
+import Adimin_Cond.repository.VeiculoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class VeiculoService {
+
+    private final VeiculoRepository veiculoRepository;
+
+    @Transactional
+    public VeiculoResponseDTO salvarVeiculo(VeiculoRequestDTO veiculoDTO)
+    {
+        Veiculo veiculo = veiculoDTO.toVeiculo();
+
+        if(this.veiculoRepository.existsByPlaca(veiculoDTO.placa()))
+        {
+            throw new PlacaRepetidaException("Placa já está cadastrada");
+        }
+
+        veiculo.setPlaca(limparPlaca(veiculoDTO.placa()));
+        veiculo.setStatus(StatusVeiculo.ATIVO);
+        this.veiculoRepository.save(veiculo);
+
+        return VeiculoResponseDTO.fromVeiculo(veiculo);
+    }
+
+    @Transactional
+    public VeiculoResponseDTO atualizarVeiculo(Long id, VeiculoUpdateRequestDTO veiculoUpdateDTO)
+    {
+        Veiculo veiculo = this.veiculoRepository.findById(id).orElseThrow(()->new IdNaoEncontradoException("ID de veiculo não encontrado"));
+        veiculoUpdateDTO.updateVeiculo(veiculo);
+
+        this.veiculoRepository.save(veiculo);
+
+        return VeiculoResponseDTO.fromVeiculo(veiculo);
+    }
+
+    public VeiculoResponseDTO buscarPlaca(String placa)
+    {
+        Veiculo veiculo = this.veiculoRepository.findByPlaca(placa);
+
+        if(veiculo == null)
+        {
+            throw new PlacaRepetidaException("Placa não encontrada");
+        }
+
+        return VeiculoResponseDTO.fromVeiculo(veiculo);
+    }
+
+    public List<VeiculoResponseDTO> listarTodos()
+    {
+        List<Veiculo> veiculos = this.veiculoRepository.findAll();
+
+        if(veiculos.isEmpty())
+        {
+            throw new NenhumCadastroException("Nenhum veículo cadastrado");
+        }
+
+        return veiculos.stream().map(VeiculoResponseDTO::fromVeiculo).toList();
+    }
+
+    public List<VeiculoResponseDTO> listarPorStatus(StatusVeiculo statusVeiculo)
+    {
+        List<Veiculo> veiculos = this.veiculoRepository.findByStatus(statusVeiculo);
+
+        if(veiculos.isEmpty())
+        {
+            throw new NenhumCadastroException("Nenhum veículo com esse status");
+        }
+
+        return veiculos.stream().map(VeiculoResponseDTO::fromVeiculo).toList();
+    }
+
+    public VeiculoResponseDTO veiculoID(Long id)
+    {
+        Veiculo veiculo = this.veiculoRepository.findById(id).orElseThrow(()->new IdNaoEncontradoException("ID de veiculo não encontrado"));
+        return VeiculoResponseDTO.fromVeiculo(veiculo);
+    }
+
+    //Exclusão via Soft Delete
+    public VeiculoResponseDTO desativarVeiculo(Long id)
+    {
+        Veiculo veiculo = this.veiculoRepository.findById(id).orElseThrow(()->new IdNaoEncontradoException("ID de veiculo não encontrado"));
+
+        if(veiculo.getStatus() == StatusVeiculo.INATIVO)
+        {
+            throw new VeiculoInativoException("Veículo já está inativo");
+        }
+
+        veiculo.setStatus(StatusVeiculo.INATIVO);
+
+        this.veiculoRepository.save(veiculo);
+
+        return VeiculoResponseDTO.fromVeiculo(veiculo);
+    }
+
+    // //------------ METODOS AUXILIARES ------------
+
+    public String limparPlaca(String placa)
+    {
+        return placa.replaceAll("[^A-Za-z0-9]","").toUpperCase();
+    }
 }
