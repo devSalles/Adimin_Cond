d48bd564033caf53b1f6cf69e020b5e05d4d3341
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-02-01 14:56:33 -0300

    Desenvolvimento de DTO de atualização de veículo + retirada de campo de status na DTO de request

--- Adimin_Cond/src/main/java/Adimin_Cond/dto/veiculo/VeiculoRequestDTO.java
+++ Adimin_Cond/src/main/java/Adimin_Cond/dto/veiculo/VeiculoRequestDTO.java
@@ -8,45 +8,27 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-import java.util.ArrayList;
 
 public record VeiculoRequestDTO(
 
-        @NotBlank(message = "Placa do veículo obrigatória")
+        @NotNull(message = "Cor obrigatória") @NotBlank(message = "Placa do veículo obrigatória")
         @Pattern(regexp = "^([A-Z]{3}-[0-9]{4}|[A-Z]{3}[0-9][A-Z][0-9]{2})$",
                 message = "Placa inválida. Use formato antigo (ABC-1234) ou Mercosul (ABC1D23)")
         String placa,
 
-        @NotBlank(message = "Modelo obrigatório")
+        @NotNull(message = "Cor obrigatória") @NotBlank(message = "Modelo obrigatório")
         String modelo,
 
-        @NotBlank(message = "Cor obrigatória")
-        String cor,
-
-        @NotNull(message = "Status obrigatório")
-        @Enumerated(EnumType.STRING)
-        StatusVeiculo status,
-
-        Long IdMorador
+        @NotNull(message = "Cor obrigatória") @NotBlank(message = "Cor obrigatória")
+        String cor
 ) {
-    public Veiculo toVeiculo(Morador morador)
+    public Veiculo toVeiculo()
     {
         Veiculo veiculo = new Veiculo();
 
         veiculo.setPlaca(this.placa);
         veiculo.setModelo(this.modelo);
         veiculo.setCor(this.cor);
-        veiculo.setStatus(this.status);
-
-        if(morador != null)
-        {
-            if(morador.getVeiculos() == null)
-            {
-                morador.setVeiculos(new ArrayList<>());
-            }
-
-            veiculo.setMorador(morador);
-        }
 
         return veiculo;
     }
--- Adimin_Cond/src/main/java/Adimin_Cond/dto/veiculo/VeiculoUpdateRequestDTO.java
+++ Adimin_Cond/src/main/java/Adimin_Cond/dto/veiculo/VeiculoUpdateRequestDTO.java
@@ -1,4 +1,31 @@
 package Adimin_Cond.dto.veiculo;
 
-public record VeiculoUpdateRequestDTO() {
+import Adimin_Cond.Enum.StatusVeiculo;
+import Adimin_Cond.entity.Veiculo;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record VeiculoUpdateRequestDTO(
+
+        @NotBlank(message = "Modelo obrigatório")
+        String modelo,
+
+        @NotBlank(message = "Cor obrigatória")
+        String cor,
+
+        @NotNull(message = "Status obrigatório")
+        @Enumerated(EnumType.STRING)
+        StatusVeiculo status
+) {
+    public Veiculo updateVeiculo(Veiculo veiculo)
+    {
+        veiculo.setModelo(this.modelo);
+        veiculo.setCor(this.cor);
+        veiculo.setStatus(this.status);
+
+        return veiculo;
+    }
 }
