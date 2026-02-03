4bc583b89ffe56f5b222a0a5be861a4e69fb88fd
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-02-03 19:38:34 -0300

    Criação de repositorio + enum de acesso + exceção de data para realizar busca por acesso + criação de DTO de request e response além do service e da controller

---
+++ Adimin_Cond/src/main/java/Adimin_Cond/controller/AcessoController.java
@@ -0,0 +1,4 @@
+package Adimin_Cond.controller;
+
+public class AcessoController {
+}
---
+++ Adimin_Cond/src/main/java/Adimin_Cond/core/exception/DataException.java
@@ -0,0 +1,7 @@
+package Adimin_Cond.core.exception;
+
+public class DataException extends RuntimeException {
+  public DataException(String message) {
+    super(message);
+  }
+}
